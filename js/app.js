adcae40c9ddf87de0684da88c41ddc43f102fd58
juanmarchant <jua.marchant@duocuc.cl>
2024-08-26 21:51:52 -0400

    display and clean

--- js/app.js
+++ js/app.js
@@ -2,18 +2,79 @@
 
 
 
+const display = document.querySelector('.display');
+
+let firstNumber = '';
+let symbol = '';
+let secondNumber = '';
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+};
+
+function substract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+};
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+};
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+};
 
 
+function operate(operator, firstNumber, secondNumber) {
+    // call any function
+}
 
+function updateHTML(value, type) {
+
+
+    if (secondNumber === '' && symbol === '' && type === 'number') {
+        firstNumber += value
+        display.textContent += value
+    }
+
+    if (firstNumber !== '' && type === 'symbol' && value !== '=') {
+        if (symbol === '+' || symbol === '-' || symbol === 'x' || symbol === '÷') {
+            return;
+        }
+        symbol += value;
+        display.textContent += ' ' + value + ' '
+    }
+
+    if (firstNumber !== '' && symbol !== '' && type === 'number') {
+        secondNumber += value
+        display.textContent += value
+    }
+
+    console.log(display.textContent.trim().length, firstNumber, symbol, secondNumber)
+}
+
+function clearDisplay() {
+    //Reset
+    display.textContent = ''
+    firstNumber = '';
+    secondNumber = '';
+    symbol = '';
+    return;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const calculator = document.querySelector('.col');
 
     calculator.addEventListener('click', (e) => {
+        updateHTML(e.target.textContent, e.target.classList[0])
 
         if (e.target.className === 'number') {
-            console.log(e.target.textContent)
+            operate()
+        }
+
+        if (e.target.textContent === 'C') {
+            clearDisplay();
         }
     })
 
